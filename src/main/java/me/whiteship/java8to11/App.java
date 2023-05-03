385d53e0ca7d01ade6ad355a39da8372d904efa4
yh725 <yh725@kakao.com>
2023-05-03 20:22:51 +0900

    Stream - Stream 소개 - Stream API

--- src/main/java/me/whiteship/java8to11/App.java
+++ src/main/java/me/whiteship/java8to11/App.java
@@ -1,54 +1,40 @@
 package me.whiteship.java8to11;
 
-import java.util.*;
-import java.util.function.Function;
-import java.util.function.Supplier;
-import java.util.function.UnaryOperator;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class App {
 
 	public static void main(String[] args) {
-		Foo2 foo = new DefaultFoo("Lee");
-		foo.printName();
-		foo.printNameUpperCase();
+		List<String> names = new ArrayList<>();
+		names.add("lee");
+		names.add("whiteship");
+		names.add("toby");
+		names.add("foo");
 
-		Foo2.printAnything();
-
-		List<String> name = new ArrayList<>();
-		name.add("lee");
-		name.add("whiteship");
-		name.add("foo");
+//		Stream<String> stringStream = names.stream().map(String::toUpperCase);
+		List<String> collect = names.stream().map((s) -> {
+			System.out.println(s);
+			return s.toUpperCase();
+		}).collect(Collectors.toList());
+		collect.forEach(System.out::println);
 
 		System.out.println("=====");
 
-		name.forEach(System.out::println);
+		names.forEach(System.out::println);
 
-		for (String n : name) {
-			System.out.println(n);
+		for (String name : names) {
+			if (name.startsWith("l")) {
+				System.out.println(name.toUpperCase());
+			}
 		}
 
-		System.out.println("=====");
-
-		Spliterator<String> spliterator = name.spliterator();
-		Spliterator<String> spliterator1 = spliterator.trySplit();
-		while (spliterator.tryAdvance(System.out::println));
-		System.out.println("=====");
-		while (spliterator1.tryAdvance(System.out::println));
-
-		long l = name.stream().map(String::toUpperCase)
-				.filter(s -> s.startsWith("L"))
-				.count();
-
-		System.out.println(l);
-
-//		name.removeIf(s -> s.startsWith("l"));
-
-		name.sort(String::compareToIgnoreCase);
-		name.forEach(System.out::println);
-
-		Comparator<String> compareToIgnoreCase = String::compareToIgnoreCase;
-		name.sort(compareToIgnoreCase.reversed());
-
-		name.forEach(System.out::println);
+		//병렬처리
+		List<String> collect1 = names.parallelStream().map((s) -> {
+			System.out.println(s + " " + Thread.currentThread().getName());
+			return s.toUpperCase();
+		}).collect(Collectors.toList());
 	}
 }
