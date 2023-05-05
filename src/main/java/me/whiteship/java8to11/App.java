87ff67f5dd635e1be83a5a86ffcd0508fbb1024d
yh725 <yh725@kakao.com>
2023-05-05 12:05:12 +0900

    Date와 Time - Date와 Time 소개 - Date와 Time API

--- src/main/java/me/whiteship/java8to11/App.java
+++ src/main/java/me/whiteship/java8to11/App.java
@@ -1,78 +1,88 @@
 package me.whiteship.java8to11;
 
-import java.time.Duration;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-import java.util.OptionalInt;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+import java.text.SimpleDateFormat;
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.TimeZone;
 
 public class App {
 
-	public static void main(String[] args) {
-		List<OnlineClass> springClasses = new ArrayList<>();
-		springClasses.add(new OnlineClass(1, "spring boot", true));
-		springClasses.add(new OnlineClass(2, "spring data jpa", true));
-		springClasses.add(new OnlineClass(3, "spring mvc", false));
-		springClasses.add(new OnlineClass(4, "spring core", false));
-		springClasses.add(new OnlineClass(5, "rest api development", false));
+	public static void main(String[] args) throws InterruptedException {
+		Date date = new Date();
+//		Calendar calendar = new GregorianCalendar();
+//		SimpleDateFormat dateFormat = new SimpleDateFormat();
 
-		OnlineClass spring_boot = new OnlineClass(1, "spring boot", true);
-//		Duration studyDuration = spring_boot.getProgress().getStudyDuration();
-//		System.out.println("studyDuration = " + studyDuration);
+		long time = date.getTime();
+		System.out.println(date);
+		System.out.println(time);
 
-//		Progress progress = spring_boot.getProgress();
-//		if (progress != null) {
-//			System.out.println("progress.getStudyDuration() = " + progress.getStudyDuration());
-//		}
+		Thread.sleep(1000 * 3);
+		Date after3Seconds = new Date();
+		System.out.println(after3Seconds);
+		after3Seconds.setTime(time);
+		System.out.println(after3Seconds);
 
-		Optional<Progress> progress = spring_boot.getProgress();
-		progress.ifPresent(p -> System.out.println(p.getStudyDuration()));
+		Calendar leeBirthDay = new GregorianCalendar(1990, Calendar.JULY, 25);
+		System.out.println(leeBirthDay.getTime());
+		leeBirthDay.add(Calendar.DAY_OF_YEAR, 1);
+		System.out.println(leeBirthDay.getTime());
 
-		Optional.of(10);
-		OptionalInt.of(10);
+		Instant instant = Instant.now(); // 기준시 UTC, GMT
+		System.out.println(instant);
+		System.out.println(instant.atZone(ZoneId.of("UTC")));
 
-		List<OnlineClass> springClasses2 = new ArrayList<>();
-		springClasses2.add(new OnlineClass(1, "spring boot", true));
-		springClasses2.add(new OnlineClass(2, "spring data jpa", true));
+		ZoneId zone = ZoneId.systemDefault();
+		System.out.println(zone);
+		ZonedDateTime zonedDateTime = instant.atZone(zone);
+		System.out.println(zonedDateTime);
 
-		Optional<OnlineClass> optional = springClasses2.stream()
-				.filter(oc -> oc.getTitle().startsWith("spring"))
-				.findFirst();
+		LocalDateTime now = LocalDateTime.now();
+		System.out.println(now);
 
-		boolean present = optional.isPresent();
-		System.out.println("present = " + present);
+		LocalDateTime birthDay = LocalDateTime.of(1990, Month.JULY, 25, 15, 30, 0);
+		System.out.println(birthDay);
 
-		if (optional.isPresent()) {
-			OnlineClass onlineClass = optional.get();
-			System.out.println("onlineClass = " + onlineClass.getTitle());
-		}
+		ZonedDateTime nowInKorea = ZonedDateTime.now(ZoneId.of(("Asia/Seoul")));
+		System.out.println(nowInKorea);
 
-		optional.ifPresent(oc -> System.out.println(oc.getTitle()));
-		OnlineClass onlineClass1 = optional.orElse(createNewClass());
-		OnlineClass onlineClass2 = optional.orElseGet(() -> createNewClass());
-		OnlineClass onlineClass3 = optional.orElseGet(App::createNewClass);
-		OnlineClass onlineClass4 = optional.orElseThrow(() -> {
-			return new IllegalStateException();
-		});
-		OnlineClass onlineClass5 = optional.orElseThrow(IllegalStateException::new);
-		Optional<OnlineClass> onlineClass = optional
-				.filter(oc -> !oc.isClosed());
+		ZonedDateTime zonedDateTime1 = instant.atZone(ZoneId.of("Asia/Seoul"));
+		System.out.println(zonedDateTime1);
 
-		System.out.println("onlineClass.isEmpty() = " + onlineClass.isEmpty());
+		LocalDate today = LocalDate.now();
+		LocalDate thisYearBirthDay = LocalDate.of(2023, Month.JULY, 25);
+		Period period = Period.between(today, thisYearBirthDay);
+		System.out.println(period.getDays());
 
-		Optional<Integer> integer = optional.map(OnlineClass::getId);
-		System.out.println("integer.isPresent() = " + integer.isPresent());
+		Period until = today.until(thisYearBirthDay);
+		System.out.println(until.get(ChronoUnit.DAYS));
 
-		Optional<Optional<Progress>> progress1 = optional.map(OnlineClass::getProgress);
-		Optional<Progress> progress2 = progress1.orElse(Optional.empty());
+		Instant now1 = Instant.now();
+		Instant plus = now1.plus(10, ChronoUnit.SECONDS);
+		Duration between = Duration.between(now1, plus);
+		System.out.println(between.getSeconds());
 
-		Optional<Progress> progress3 = optional.flatMap(OnlineClass::getProgress);
-	}
+		DateTimeFormatter MMddyyyy = DateTimeFormatter.ofPattern("MM/dd/yyyy");
+		System.out.println(now.format(MMddyyyy));
 
-	private static OnlineClass createNewClass() {
-		System.out.println("creating new online class");
-		return new OnlineClass(10, "New class", false);
+		LocalDate parse = LocalDate.parse("07/25/1990", MMddyyyy);
+		System.out.println(parse);
+
+		Date date1 = new Date();
+		Instant instant1 = date1.toInstant();
+		Date newDate = Date.from(instant1);
+
+		GregorianCalendar gregorianCalendar = new GregorianCalendar();
+		LocalDateTime dateTime = gregorianCalendar.toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime();
+		ZonedDateTime zonedDateTime2 = gregorianCalendar.toInstant().atZone(ZoneId.systemDefault());
+		GregorianCalendar from = GregorianCalendar.from(zonedDateTime2);
+
+		ZoneId zoneId = TimeZone.getTimeZone("PST").toZoneId();
+		TimeZone timeZone = TimeZone.getTimeZone(zoneId);
+
+		LocalDateTime plus1 = now.plus(10, ChronoUnit.DAYS);
 	}
 }
